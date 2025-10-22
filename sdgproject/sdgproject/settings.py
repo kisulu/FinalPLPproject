624311ab31a76309d4337ff526f88d46e5aa5b39
Benedict Ooko <benramo254@gmail.com>
2025-10-22 14:18:44 +0300

    Styling on home page and other pages

--- sdgproject/sdgproject/settings.py
+++ sdgproject/sdgproject/settings.py
@@ -123,7 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # This is where you’ll put your static/ folder in your project root
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # For collectstatic (useful in production)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
